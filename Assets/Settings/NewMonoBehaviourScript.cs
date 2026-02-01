11d58b8f13048d646b3f7c1479f7b0f38b040903
Savannah Lau <slau24@illinois.edu>
2026-02-01 17:41:55 -0600

    Initial commit

--- Assets/Settings/NewMonoBehaviourScript.cs
+++ Assets/Settings/NewMonoBehaviourScript.cs
@@ -1,6 +1,6 @@
 using UnityEngine;
 
-public class Quit : MonoBehaviour
+public class NewMonoBehaviourScript : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
